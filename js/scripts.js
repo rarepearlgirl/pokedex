2cacd1dc4ad977b12da072bf8d77191a5c424c00
Eva Nikolaieva <draculahgirl@gmail.com>
2022-12-08 17:28:15 +0100

    create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+let pokemonList = [
+  { 
+    name: "Bulbasaur",
+    attack: 49,
+    HP: 45,
+    height: 0.7,
+    number: 1,
+    type: ["grass", "poison"]
+ },
+  {
+    name: "Squirtle",
+    attack: 48,
+    HP: 44,
+    height: 0.5,
+    number: 7,
+    type: "Water",
+  },
+  {
+    name: "Ponyta",
+    attack: 85,
+    HP: 50,
+    height: 1,
+    number: 77,
+    type: "fire"
+  },
+  
+  {
+    name: "Sharpedo",
+    attack: 120,
+    HP: 70,
+    height: 1.8,
+    number: 319,
+    type: ["dark", "water"]
+  },
+
+];
